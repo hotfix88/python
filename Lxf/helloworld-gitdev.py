869f45067f529ab8ecefb99714ca6b234716c25d
hotfix88 <hotfix88@sina.com>
2016-11-09 14:23:40 +0800

    modify helloworld.py ,,ADD helloworld-gitdev.py by git-dev

--- Lxf/helloworld-gitdev.py
+++ Lxf/helloworld-gitdev.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 09 14:10:01 2016
+Created on Wed Nov 09 14:21:37 2016
 @author: Administrator
 created by CMCC.fengyang
 fyso@163.com
 """
 
-print 'hello, world!'
-print 'Git-dev!'
+print 'hello git-dev!'

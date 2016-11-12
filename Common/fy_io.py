c81b438e3114da39d0449e4e8ffab14010c364e0
hotfix88 <hotfix88@sina.com>
2016-11-12 20:47:45 +0800

    temp

--- Common/fy_io.py
+++ Common/fy_io.py
@@ -9,21 +9,38 @@ Created on Wed Nov 09 23:05:56 2016   @Administrator
 
 #this file is for output and input!
 
-#------------1.input a tsv file-------------
-f = 'D:\\Git\\python\\PCI\\chapter3\\1.txt'
-for line in file(f):
-    print line
-lines = [line for line in file(f)]
-print lines;
 
-colnames=lines[0].strip().split('\t')[1:]
-rownames=[]
-data=[]
-for line in lines[1:]:
-    p=line.strip().split('\t')
+#-------------------------------------
+def readfile(filename,sp='\t'):
+  lines=[line for line in file(filename)]
+  
+  # First line is the column titles
+  colnames=lines[0].strip().split(sp)[1:]
+  rownames=[]
+  data=[]
+  for line in lines[1:]:
+    p=line.strip().split(sp)
     # First column in each row is the rowname
     rownames.append(p[0])
     # The data for this row is the remainder of the row
     data.append([float(x) for x in p[1:]])
-
-print data[0],data[1]
+  return rownames,colnames,data
+  
+#------------1.input a tsv file-------------
+#f1 = 'D:\\Git\\python\\PCI\\chapter3\\1.txt'
+#for line in file(f1):
+#    print line
+#lines = [line for line in file(f1)]
+#print lines;
+#
+#colnames=lines[0].strip().split('\t')[1:]
+#rownames=[]
+#data=[]
+#for line in lines[1:]:
+#    p=line.strip().split('\t')
+#    # First column in each row is the rowname
+#    rownames.append(p[0])
+#    # The data for this row is the remainder of the row
+#    data.append([float(x) for x in p[1:]])
+#
+#print data[0],data[1]
---
+++ Common/fy_io_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 20:35:32 2016  @author: Administrator
+
+@author: deepblue
+@GitHub: hotfix88
+@Email : hotfix88@sina.com
+"""
+import fy_io
+
+f1 = 'fy_io_1.txt'
+f2 = 'fy_io_2.txt'

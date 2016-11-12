88c749fa65b90d0dc9a810ec4b2dca8c9dea3cd0
hotfix88 <hotfix88@sina.com>
2016-11-12 20:32:29 +0800

    modify common file

---
+++ Common/fy_dm_cluster.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 11:55:28 2016  @author: Administrator
+
+@author: deepblue
+@GitHub: hotfix88
+@Email : hotfix88@sina.com
+
+this file is for my cluster study and test!
+"""
+

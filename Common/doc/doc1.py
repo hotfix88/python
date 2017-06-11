421d0eb09fb84775df1190b895e01e779f5f8aaa
hotfix88 <hotfix88@sina.com>
2017-06-11 03:30:29 -0700

    add 20170611

--- Common/doc/doc1.py
+++ Common/doc/doc1.py
@@ -8,18 +8,19 @@ Created on Fri Jun 09 15:42:37 2017
 #增加字段的函数
 
 def readfile1(filename,sp='\t'):
-  lines=[line for line in file(filename)]
-  lines2=[]
-  for line in lines:
-      line = 'JSDM.' + line[:-1] + '@BASCM' + '\n'
-      lines2.append(line)
-  return lines2
+
+	lines=[line for line in file(filename)]
+	lines2=[]
+	for line in lines:
+		line = 'JSDM.' + line[:-1] + '@BASCM' + '\n'
+		lines2.append(line)
+	return lines2
 
 
-doc = readfile1('D://doc1_input.txt')
+doc = readfile1('./doc1_input.txt')
 
-f = file('D://doc1_output.txt','w+')
+f = file('./doc1_output.txt','w+')
 for line in doc:
-    f.write(line)
+	f.write(line)
 f.close()
 

26a90b1fa6df3d29af5116424b9300cd683dd50a
hotfix88 <hotfix88@sina.com>
2017-06-11 21:07:40 +0800

    modi

--- common/doc/doc1.py
+++ common/doc/doc1.py
@@ -9,12 +9,18 @@ Created on Fri Jun 09 15:42:37 2017
 
 def readfile1(filename,sp='\t'):
 
-	lines=[line for line in file(filename)]
-	lines2=[]
-	for line in lines:
-		line = 'JSDM.' + line[:-1] + '@BASCM' + '\n'
-		lines2.append(line)
-	return lines2
+    lines=[line for line in file(filename)]
+    lines2=[]
+    for line in lines:
+        line = 'JSDM.' + line[:-1] + '@BASCM' + '\n'
+        lines2.append(line)
+    return lines2
+    
+	
+def readfile():
+	print 1
+	
+ 
 
 
 doc = readfile1('doc1_input.TXT')

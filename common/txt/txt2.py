d9db8808b44d16a9398385ac89de5315efd4b5dc
hotfix88 <hotfix88@sina.com>
2017-06-12 23:02:51 +0800

    modi

--- common/txt/txt2.py
+++ common/txt/txt2.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun 12 22:37:22 2017
+Created on Mon Jun 12 22:55:03 2017
 
 @author: fyso
 """
 
-#按行增加字符处理！
+#大小写转化
 
 def readfile1(filename,sp='\t'):
-
     lines=[line for line in file(filename)]
     lines2=[]
     linenum = 0
     for line in lines:
-        line = 'JSDM.' + line[:-1] + '@BASCM' + '\n'
-        lines2.append(line)
-        linenum += 1
+        lines2.append(line.upper())
+#        lines2.append(line.lower())
+        linenum +=1
     return lines2,linenum
 
-
-
-infilename = 'txt1.txt'
-
+infilename = 'txt2.txt'
 doc,num = readfile1(infilename)
 outfilename = 'out_'+infilename
-
-#'D:/anadata/'
 
 f = file(outfilename,'w+')
 for line in doc:
     f.write(line)
 print 'create file : ' , outfilename ,  ',and ',num,' lines!'
 
-
 f.close()
-
-
-

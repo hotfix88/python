faa95e0c52fa2f933834e9338deacc5824a36245
hotfix88 <hotfix88@sina.com>
2017-08-26 13:44:12 +0800

    modi 20170820 homej

---
+++ test/22/1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ Author:      fyso
+ DateTime:    2017-08-25 11:24:36
+ Description: Description
+"""
+
+
+mv  = [1,3,3]
+print mv
+print 55
+
+

853b1d65c372f98bd374ad868ced3f730b74aa8f
hotfix88 <hotfix88@sina.com>
2017-12-22 19:24:55 +0800

    add PSC dictionary

---
+++ PSC/ch01/01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ Author:      fyso@163.com fengyangsgs@js.chinamobile.com
+ DateTime:    Fri Dec 22 12:37:59 2017
+ Description: Description
+"""
+__author__ = 'FengYang'
+
+
+%matplotlib inline
+import pylab as pl
+pl.seed(1)
+data = pl.randn(100)
+pl.plot(data)

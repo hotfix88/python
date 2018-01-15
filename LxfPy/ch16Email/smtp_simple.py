e4993d1e7bc092bba426dcdb01f6002d2cbbb214
hotfix88 <hotfix88@sina.com>
2018-01-15 10:27:35 +0800

    add

--- LxfPy/ch16Email/smtp_simple.py
+++ LxfPy/ch16Email/smtp_simple.py
@@ -12,13 +12,31 @@ __author__ = 'FengYang'
 
 from email.mime.text import MIMEText
 
+
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+        Header(name, 'utf-8').encode(), \
+        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+    
+    
+    
+
 msg = MIMEText('您好，电渠数据分析平台账号密码已经修改 : xw#86tP', 'plain', 'utf-8')
 
 
 
 from_addr = 'fengyangsgs@js.chinamobile.com'
 
-password = 'Fy955785'
+password = 'Fy955787'
+
+
 
 
 smtp_server = '172.16.121.102'
@@ -27,7 +45,11 @@ smtp_server = '172.16.121.102'
 to_addr = 'fengyangsgs@js.chinamobile.com'
 
 
-# msg['Subject'] = Header(u'你好，你的密码已经修改', 'utf-8').encode()
+msg['Subject'] = Header(u'你好，你的密码已经修改', 'utf-8').encode()
+
+msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+
 
 import smtplib
 server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25

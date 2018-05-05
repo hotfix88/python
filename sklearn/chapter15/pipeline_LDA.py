bf2e6f865069404ecd1ce6aef89ba9354658e815
hotfix88 <hotfix88@sina.com>
2018-05-05 20:48:02 +0800

    add chapter15  pipeline

---
+++ sklearn/chapter15/pipeline_LDA.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ Author:      fyso@163.com
+ DateTime:    Sat May  5 20:42:24 2018
+ Description: Description
+"""
+__author__ = 'Fyso'
+
+from pandas import read_csv
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.pipeline import  Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+# 导入数据
+filename = 'D:\Git\python\sklearn\data\pima_data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+# 将数据分为输入数据和输出结果
+array = data.values
+X = array[:, 0:8]
+Y = array[:, 8]
+
+
+num_folds = 10
+seed = 7
+kfold = KFold(n_splits=num_folds, random_state=seed)
+steps = []
+
+# 创建Pipeline
+steps.append(('Standardize', StandardScaler()))
+steps.append(('lda', LinearDiscriminantAnalysis()))
+model = Pipeline(steps)
+result = cross_val_score(model, X, Y, cv=kfold)
+print(result.mean())
